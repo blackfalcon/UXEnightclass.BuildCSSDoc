9680ef998c9e2b64d5f4ad2a163f6892bad83798
igotsideas <ardonbailey@gmail.com>
2014-02-13 12:53:31 -0800

    Add CSS to the HTML

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // set variables for environment
 var express = require('express');
 var app = express();
@@ -8,6 +9,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade'); // use either jade or ejs       // instruct express to server up static assets
 app.use(express.static('public'));
 
+// set routes
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
 // Set server port
-app.listen(4000);
-console.log('server is running');
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log("The server is now listening on port %s", port);
